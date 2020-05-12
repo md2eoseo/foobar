064256102c9db82314d98f429253430e5a4da7ff
md2eoseo <md2eoseo@gmail.com>
2020-05-12 17:30:06 +0200

    getting data from backend team

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const DB_URL = "https://neonapp.herokuapp.com/";
+
 function App() {
-  return <div>Foo Bar</div>;
+  useEffect(() => get(), []);
+
+  function get() {
+    fetch(DB_URL, {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        // "x-apikey": API_KEY,
+        // Request header field cache-control is not allowed by Access-Control-Allow-Headers in preflight response.
+        // "cache-control": "no-cache",
+      },
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        else console.log("GET failed!");
+      })
+      .then((data) => {
+        // update state here
+        console.log(`fetched following data -> ${JSON.stringify(data)}`);
+      });
+  }
+
+  return <div className="App">Foo Bar</div>;
 }
 
 export default App;
